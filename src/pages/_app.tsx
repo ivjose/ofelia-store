6055486ff428dcc15e92b6f87aa5f7b2a96171af
“Jose <ivjosesantos@gmail.com>
2022-05-11 22:04:14 +0800

    Tailwind setup and folder adjustment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const data = 'Test asdsad asdasdas'
-
   return <Component {...pageProps} />
 }
 
