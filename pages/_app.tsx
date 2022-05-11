d0f526e08531d004feccdf879d346f27e391f8fe
“Jose <ivjosesantos@gmail.com>
2022-05-11 21:51:27 +0800

    check error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const data = 'Test asdsad'
+  const data = 'Test asdsad asdasdas'
 
   return <Component {...pageProps} />
 }
